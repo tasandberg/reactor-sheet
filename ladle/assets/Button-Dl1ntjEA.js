ffcd9cb97f16a83b6b34e12e8ba928218a4c32e8
tasandberg <tasandberg@users.noreply.github.com>
2026-06-27 19:14:08 +0000

    deploy: c4049e6dba8f5a7de718ee0858c7d847dd2280df

---
+++ ladle/assets/Button-Dl1ntjEA.js
@@ -0,0 +1,1 @@
+import{j as u,c as a}from"./index-BH6R7HkT.js";function c({variant:t,size:s,className:n,type:o="button",...r}){return u.jsx("button",{type:o,className:a("btn",t,s,n),...r})}export{c as B};
