f218a7cdcd7e4b05d9c9eadb20a5a57dbb1a27e7
Tim Sandberg <tim.sandberg@monograph.com>
2026-06-27 11:21:10 -0700

    refactor: drop dead WealthMovement component and unused _util.scss

--- src/ReactorSheet/components/actions/index.ts
+++ src/ReactorSheet/components/actions/index.ts
@@ -2,4 +2,3 @@ export { ActionsView } from "./ActionsView";
 export { AbilityPlaques } from "./AbilityPlaques";
 export { AttacksTable } from "./AttacksTable";
 export { SavesExploration } from "./SavesExploration";
-export { WealthMovement } from "./WealthMovement";
