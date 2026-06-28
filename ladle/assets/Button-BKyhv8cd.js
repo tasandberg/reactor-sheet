45ad5cfabee43015e388136dfddfe7f37baf41cf
tasandberg <tasandberg@users.noreply.github.com>
2026-06-28 05:12:25 +0000

    deploy: 2c71594ff195b2707f02d93d24379a5051b72ab7

---
+++ ladle/assets/Button-BKyhv8cd.js
@@ -0,0 +1,1 @@
+import{j as u,c as a}from"./index-D_y1l8RJ.js";function c({variant:t,size:s,className:n,type:o="button",...r}){return u.jsx("button",{type:o,className:a("btn",t,s,n),...r})}export{c as B};
---
+++ ladle/assets/Stamp-CEa-D8zq.js
@@ -0,0 +1,1 @@
+import{j as m,c as p}from"./index-D_y1l8RJ.js";function n({size:s="sm",className:a,...t}){return m.jsx("span",{className:p("stamp",s,a),...t})}export{n as S};
